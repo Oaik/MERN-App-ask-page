aaa12ceaf3e391e1c5528a137a7b70c58c080f20
omar ashraf <omarashrf15@gmail.com>
2018-11-29 20:49:46 -0800

    Completed ServerSide and question schema

--- server/schema/Post.js
+++ server/schema/Post.js
@@ -1,2 +1,28 @@
-// Question or answer -> text/string
-// post id -> Number
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const postSchema = new Schema({
+    body: {
+        type: String,
+        required: true
+    },
+    userAsked: {
+        type: String,
+        required: true
+    },
+    allowName: {
+        type: Boolean, 
+        required: true
+    },
+    userAsking: {type: String},
+    questionAnswered: {
+        type: Boolean,
+        default: false 
+    },
+    answer: {
+        type: String
+    }
+})
+
+const Post = mongoose.model('post', postSchema);
+module.exports = Post;
